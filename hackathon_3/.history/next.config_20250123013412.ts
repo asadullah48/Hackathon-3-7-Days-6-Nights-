8f05edb2d42cf42b912af093dfa395dce544ef98
Asadullah Shafique <asadullahshafique@hotmail.com>
2025-01-23 02:11:52 +0500

    small error that caused me 4 hrs

---
+++ hackathon_3/.history/next.config_20250123013412.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+    // If you're having type issues during build, you can temporarily set this to true
+    ignoreBuildErrors: false,
+  },
+  // Add this if you're using the Sanity Visual Studio Code extension
+  experimental: {
+    esmExternals: 'loose',
+  }
+};
+
+export default nextConfig;
