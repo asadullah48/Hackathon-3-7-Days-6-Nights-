11afc505a3a7d0b35d3c2831bf89eabbf901e5a2
Asadullah Shafique <asadullahshafique@hotmail.com>
2025-02-03 00:11:13 +0500

    signing out for today

---
+++ .history/hackathon_3/src/app/page_20250114114540.tsx
@@ -0,0 +1,20 @@
+import Blog from "./Components/Blog";
+import HeroSection from "./Components/HeroSection";
+import NewArrivals from "./Components/NewArrivals";
+import TableSection from "./Components/TableSection";
+import TopPicks from "./Components/TopPicks";
+import FollowUs from "./Components/FollowUs";
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <HeroSection/>
+      <TableSection/>
+      <TopPicks/>
+      <NewArrivals/>
+      <Blog/>
+      <FollowUs/>
+    </main>
+  );
+}
